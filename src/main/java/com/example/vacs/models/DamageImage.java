8948125cd1b0c5c92e4e1996ba7cb28600d109b6
baturon <vov.85@mail.ru>
2023-09-05 15:09:17 +0300

    05.09

---
+++ src/main/java/com/example/vacs/models/DamageImage.java
@@ -0,0 +1,17 @@
+//package com.example.vacs.models;
+//
+//
+//import jakarta.persistence.Entity;
+//import jakarta.persistence.Table;
+//import lombok.AllArgsConstructor;
+//import lombok.Data;
+//import lombok.NoArgsConstructor;
+//
+//@Entity
+//@Data
+//@AllArgsConstructor
+//@NoArgsConstructor
+//@Table(name = "damageImages")
+//public class DamageImage {
+//
+//}
